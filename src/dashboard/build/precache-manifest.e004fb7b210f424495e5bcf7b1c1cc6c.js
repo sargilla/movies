9a208f56175dcdeaeaf195f93c9feb206eb395da
Santiago Argilla <sargilla@gmail.com>
2021-06-16 18:46:56 -0300

    con build de react

---
+++ src/dashboard/build/precache-manifest.e004fb7b210f424495e5bcf7b1c1cc6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ca5090ebd70b7da10f7d9300243d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a4343330e0cac1a0304",
+    "url": "/static/css/main.35a3cf12.chunk.css"
+  },
+  {
+    "revision": "f5b674c5aa2f502cbc26",
+    "url": "/static/js/2.13c14c29.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.13c14c29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4343330e0cac1a0304",
+    "url": "/static/js/main.00ab2d6b.chunk.js"
+  },
+  {
+    "revision": "e834ae95d6375f097890",
+    "url": "/static/js/runtime-main.91c41847.js"
+  },
+  {
+    "revision": "887fe0f6e018fcb54ea219604897dc9c",
+    "url": "/static/media/dummy-avatar.887fe0f6.jpg"
+  }
+]);
